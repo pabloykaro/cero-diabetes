21efc09a2df1dc1aaaac8f75232b46c466a72837
Pablo Ykaro <pabloykaro9@gmailcom>
2022-10-17 11:53:42 -0300

    fix detect proxy v2

--- index.php
+++ index.php
@@ -5,12 +5,7 @@ function detectSpy(): bool{
   preg_match('/bot|crawl|slurp|spider|mediapartners|facebookexternalhit|facebookcatalog|facebot|googlebot/i', $_SERVER['HTTP_USER_AGENT']));
 }
 function proxy_detect () {
-$proxyports=array(80,8080,6588,8000,3128,3127,3124,1080,553,554);
-for ($i = 0; $i <= count($proxyports); $i++) {
-if(@fsockopen($_SERVER['REMOTE_ADDR'],$proxyports[$i],$errstr,$errno,0.5)){
-$sockport=true;
-}
-}
+
 if(
     $_SERVER['HTTP_FORWARDED']
 || $_SERVER['HTTP_X_FORWARDED_FOR']
@@ -18,7 +13,6 @@ if(
 || $_SERVER['HTTP_VIA']
 || $_SERVER['HTTP_XROXY_CONNECTION']
 || $_SERVER['HTTP_PROXY_CONNECTION']
-|| $sockport == true
 )
 {
 return true;
